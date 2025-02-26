2a564460efe92e9612972ae9f206f8c091e89bb8
Luis Zambrano <154038166+LuisAntonio06@users.noreply.github.com>
2025-02-26 12:31:30 -0600

    Add files via upload

---
+++ js/appRecuperar.js
@@ -0,0 +1,7 @@
+const formularioRecuperacion = document.querySelector('#formularioRecuperacion');
+
+formularioRecuperacion.addEventListener('submit' , validarFormulario);
+
+function validarFormulario(e){
+    e.preventDefault();
+}
